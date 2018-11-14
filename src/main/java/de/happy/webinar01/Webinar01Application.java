a2642fda9a698099089762fe2d5975ab16b653c6
Karsten Gresch <git-2015@by-doing.org>
2018-11-14 22:45:57 +0100

    Initial add

---
+++ src/main/java/de/happy/webinar01/Webinar01Application.java
@@ -0,0 +1,12 @@
+package de.happy.webinar01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webinar01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webinar01Application.class, args);
+	}
+}
---
+++ src/test/java/de/happy/webinar01/Webinar01ApplicationTests.java
@@ -0,0 +1,16 @@
+package de.happy.webinar01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Webinar01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
